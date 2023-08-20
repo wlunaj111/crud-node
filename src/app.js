400084e18fe8860ac435d6e550ceb3940d996a85
wlunaj111 <wlunaj111@gmail.com>
2023-08-19 23:26:05 -0400

    Add  HTTP request morgan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import express from 'express'
+import express from 'express';
+import morgan from 'morgan'
 
 
-const app = express()
+const app = express();
+app.use(morgan("dev"))
 
-
-app.listen(3000)
-
-console.log('Server on port 3000')
+export default app;
